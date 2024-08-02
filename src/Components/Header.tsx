120bca9d64a44ca585ff56036880b4e944d8189e
H3nryK <henrykimani89@gmail.com>
2024-08-02 11:21:41 +0300

    responsive header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import logo from '../assets/Laura.png';
 import { Link } from 'react-router-dom';
 
 
 export default function Header() {
-    return (
-        <header className='header'>
-        <nav className='navbar'>
-          <ul className='navbar-menu'>
-           
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+  
+  return (
+    <header className='header'>
+      <nav className='navbar'>
+        <div className='navbar-logo'>
           <img src={logo} className='logo-image' alt='logo'/>
-          
-            <li className='navbar-item'><Link to="/">Home</Link></li>
-            <li className='navbar-item'><Link to="/about">About</Link></li>
-            <li className='navbar-item'><Link to='/resume'>Resume</Link></li>
-            <li className='navbar-item'><Link to="/Contact">Contact</Link></li>
-          </ul>
-        </nav>
-      </header>
-    )
+        </div>
+        <button className='navbar-toggle' onClick={handleToggle}>
+          &#9776;
+        </button>
+        <ul className={`navbar-menu ${isNavOpen ? 'open' : ''}`}>
+          <li className='navbar-item'><Link to="/" onClick={handleToggle}>Home</Link></li>
+          <li className='navbar-item'><Link to="/about" onClick={handleToggle}>About</Link></li>
+          <li className='navbar-item'><Link to='/resume' onClick={handleToggle}>Resume</Link></li>
+          <li className='navbar-item'><Link to="/contact" onClick={handleToggle}>Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 } 

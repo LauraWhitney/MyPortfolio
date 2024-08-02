411d8151c91f091bf63dd8a79b3c73f0bad914cd
laurawhitney31@gmail.com <laurawhitney31@gmail.com>
2024-08-02 22:19:59 +0300

    Link

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import logo from '../assets/logo.jpg';
 import { Link } from 'react-router-dom';
+import Resume from '../assets/Resume.pdf';
 
 
 export default function Header() {
@@ -13,9 +14,9 @@ export default function Header() {
           <img src={logo} className='logo-image' alt='logo'/>
           
             <li className='navbar-item'><Link to="/">Home</Link></li>
-            <li className='navbar-item'><Link to="about">About</Link></li>
-            <li className='navbar-item'><a href='resume'>Resume</a></li>
-            <li className='navbar-item'><Link to="Contact">Contact</Link></li>
+            <li className='navbar-item'><Link to="/about">About</Link></li>
+            <li className='navbar-item'><a href={Resume}>Resume</a></li>
+            <li className='navbar-item'><Link to="/Contact">Contact</Link></li>
           </ul>
         </nav>
       </header>

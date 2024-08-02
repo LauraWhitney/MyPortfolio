5bb58b189d83b4ccdf186e154284228c4a392192
H3nryK <henrykimani89@gmail.com>
2024-08-02 10:39:25 +0300

    home page is better

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -8,19 +8,21 @@ import { Link } from 'react-router-dom';
 export default function Home() {
 
   return (
-    <div className='Portfolio'>
+    <div>
        <Header /> 
-      <img src={Me} className='me-image' alt='me'/>
-      <div className='container'></div>
-      
-      <h1> Hello , I'm Laura  </h1>
-      <h2>Dive in and explore my creativity !!!</h2>
+      <div className='container'>
+        <img src={Me} className='me-image' alt='me'/>
+        <div className='Intro'>
+          <h1> Hello , I'm Laura  </h1>
+          <h2>Dive in and explore my creativity !!!</h2>
+        </div>
+      </div>
       <div className='Button'>
         <Link to="/about">
-     <button type="button"><span></span>About me</button>
+          <button type="button">About me</button>
         </Link>
         <Link to="/contact">
-     <button type="button"><span></span>Contact Me</button>
+          <button type="button">Contact Me</button>
        </Link>
       </div>
       <Footer/>
